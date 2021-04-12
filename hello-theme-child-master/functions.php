9ed106474f0c2f1e8141e3914814998329f749c3
Robby <hello@rabeah.me>
2021-04-12 09:51:08 -0400

    Post form submission to 3rd part api
    Added function to post form submission from apply online form to 3rd party api (gragg)

--- hello-theme-child-master/functions.php
+++ hello-theme-child-master/functions.php
@@ -10,6 +10,41 @@
  *
  * @return void
  */
+
+ // Post Gravity form #1 (Apply Online) to Gragg
+add_action( 'gform_after_submission_1', 'post_to_third_party', 10, 2 );
+function post_to_third_party( $entry, $form ) {
+
+    $endpoint_url = 'http://services.graggadv.com/lead-gateway/';
+    $body = array(
+        'glue_id' => rgar( $entry, '22' ),
+        'campus' => rgar( $entry, '5' ),
+        'message' => rgar( $entry, '20' ),
+		'program' => rgar( $entry, '6' ),
+		'gradyear' => rgar( $entry, '12' ),
+		'zip' => rgar( $entry, '8.5' ),
+		'state' => rgar( $entry, '8.4' ),
+		'city' => rgar( $entry, '8.3' ),
+		'address2' => rgar( $entry, '8.2' ),
+		'address1' => rgar( $entry, '8.1' ),
+		'phone_day' => rgar( $entry, '3' ),
+		'email' => rgar( $entry, '2' ),
+		'lname' => rgar( $entry, '23' ),
+		'fname' => rgar( $entry, '24' ),
+		'text_opt' => rgar( $entry, '4' ),
+		'text_opt' => rgar( $entry, '4' ),
+        );
+
+	// Debug Log post
+    GFCommon::log_debug( 'gform_after_submission: body => ' . print_r( $body, true ) );
+
+	// Performs an HTTP request using the POST method and returns its response.
+    $response = wp_remote_post( $endpoint_url, array( 'body' => $body ) );
+
+	// Debug Log post response
+    GFCommon::log_debug( 'gform_after_submission: response => ' . print_r( $response, true ) );
+}
+
 function hello_elementor_child_enqueue_scripts() {
 	wp_enqueue_style(
 		'hello-elementor-child-style',
@@ -24,6 +59,9 @@ add_action( 'wp_enqueue_scripts', 'hello_elementor_child_enqueue_scripts', 20 );
 add_action('wp_head','chat_script');
 
 function chat_script()
+
+
+
 { // break out of php ?>
 
 <script type="text/javascript">
